ff198bda219e023c815d7206a114c9b5c0a37bfb
Gewrry <santiagogerry79@gmail.com>
2026-01-19 13:52:28 +0800

    Initial commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,6 +50,14 @@
                 <div>
                     <h3 class="text-xs font-semibold text-gray-500 uppercase tracking-wider mb-2">Reports</h3>
                     <div class="space-y-1">
+                        
+                        <x-nav-link href="{{ route('accounts.index') }}" :active="request()->routeIs('accounts.index')">
+                            <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+                            </svg>
+                            {{ __('Admin') }}
+                        </x-nav-link>
 
                         <x-nav-link href="{{ route('employee-info.create') }}"
                             :active="request()->routeIs('employee-info.create')">
@@ -57,17 +65,10 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
                             </svg>
-                            {{ __('Create Employee') }}
+                            {{ __('Human Resources') }}
                         </x-nav-link>
 
-                        <x-nav-link href="{{ route('accounts.index') }}"
-                            :active="request()->routeIs('accounts.index')">
-                            <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-                            </svg>
-                            {{ __('Create Account') }}
-                        </x-nav-link>
+
                     </div>
                 </div>
 
